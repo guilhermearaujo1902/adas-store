bd445215f79fb37d6523ec9883c167a79b9ec006
Guilherme Araujo Silva <guilherme.silva@PROWAY.TREINA>
2024-08-06 22:19:45 -0300

    :construction: feat(categoria): Criacao dos componentes

--- frontend/store-front/src/app/componentes/categoria/categoria-lista/categoria-lista.component.ts
+++ frontend/store-front/src/app/componentes/categoria/categoria-lista/categoria-lista.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Categoria } from '../../interfaces/categoria';
+import { CategoriaService } from '../../servicos/categoria.service';
 
 @Component({
   selector: 'app-categoria-lista',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrl: './categoria-lista.component.scss'
 })
 export class CategoriaListaComponent {
+
+  categorias: Categoria[] = [];
+
+  constructor(private categoriaService: CategoriaService) {}
+
+  ngOnInit(): void {
+    this.carregarCategorias();
+  }
+
+  carregarCategorias(): void {
+    this.categoriaService.findAll().subscribe(retorno => {
+      this.categorias = retorno;
+    });
+  }
+
+  delete(id: number): void {
+    this.categoriaService.delete(id).subscribe(() => {
+      this.carregarCategorias();
+    });
+  }
 
 }
