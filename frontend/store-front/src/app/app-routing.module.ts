ab4a53605bc5af368b42b43576fb4fbea86fb4a9
Guilherme Araujo Silva <guilherme.silva@PROWAY.TREINA>
2024-08-08 19:59:40 -0300

    :construction: feat(categoria): organizando o componente lista

--- frontend/store-front/src/app/app-routing.module.ts
+++ frontend/store-front/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriaListaComponent } from './componentes/categoria/categoria-lista/categoria-lista.component';
+import { CategoriaFormComponent } from './componentes/categoria/categoria-form/categoria-form.component';
+import { CategoriaDetalheComponent } from './componentes/categoria/categoria-detalhe/categoria-detalhe.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'categorias', component: CategoriaListaComponent},
+  {path: 'categoria/nova', component: CategoriaFormComponent},
+  {path: 'categoria/editar/:id', component: CategoriaFormComponent},
+  {path: 'categoria/:id', component: CategoriaDetalheComponent},
+  {path: '', redirectTo: '/categorias', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/store-front/src/app/app.module.ts
+++ frontend/store-front/src/app/app.module.ts
@@ -6,17 +6,25 @@ import { AppComponent } from './app.component';
 import { CategoriaListaComponent } from './componentes/categoria/categoria-lista/categoria-lista.component';
 import { CategoriaFormComponent } from './componentes/categoria/categoria-form/categoria-form.component';
 import { CategoriaDetalheComponent } from './componentes/categoria/categoria-detalhe/categoria-detalhe.component';
+import { CabecalhoComponent } from './componentes/compartilhados/cabecalho/cabecalho.component';
+import { MenuComponent } from './componentes/compartilhados/menu/menu.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     CategoriaListaComponent,
     CategoriaFormComponent,
-    CategoriaDetalheComponent
+    CategoriaDetalheComponent,
+    CabecalhoComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
