c8593ee78d45a18ee2fa0970c5429132a3b9d93d
Guilherme Araujo Silva <guilherme.silva@PROWAY.TREINA>
2024-08-08 22:17:47 -0300

    :construction: feat(categoria): Finalizando layout

--- frontend/store-front/src/app/componentes/categoria/categoria-form/categoria-form.component.ts
+++ frontend/store-front/src/app/componentes/categoria/categoria-form/categoria-form.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Categoria } from '../../interfaces/categoria';
+import { CategoriaService } from '../../servicos/categoria.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-categoria-form',
@@ -6,5 +9,41 @@ import { Component } from '@angular/core';
   styleUrl: './categoria-form.component.scss'
 })
 export class CategoriaFormComponent {
+
+  categoria: Categoria = {
+    id: 0,
+    nome: '',
+    status: false
+  };
+
+  isEdicao: boolean = false;
+
+  constructor(
+    private categoriaService: CategoriaService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    const id: number = Number(this.route.snapshot.paramMap.get('id'));
+    if (id && id != 0) {
+      this.isEdicao = true;
+      this.categoriaService.findById(id).subscribe(retorno => {
+        this.categoria = retorno;
+      });
+    }
+  }
+
+  salvar(): void {
+    if (this.isEdicao) {
+      this.categoriaService.update(this.categoria).subscribe(() => {
+        this.router.navigate(['/categorias']);
+      });
+    } else {
+      this.categoriaService.add(this.categoria).subscribe(() => {
+        this.router.navigate(['/categorias']);
+      });
+    }
+  }
 
 }
